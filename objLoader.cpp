ad9487468e9d20c25f9f85e0184585ea6a50adcf
Vanessa Truong <truonv1@mcmaster.ca>
2017-12-11 18:54:06 -0500

    obj loader

--- objLoader.cpp
+++ objLoader.cpp
@@ -19,88 +19,27 @@ objLoader::objLoader(){
 
 }
 GLfloat* objLoader::loadObjFromFile(string filename){
+	int numVertices = 0;
+	int numRecords = 24; 
+	vector<float> colour[4] = {1.0,1.0,1.0,1.0};
 
+	//3 + 4 + 2 + 3 + 4 + 4 + 1
+	// Vertex + Colour + Texture + normals + Material Specular + Material Diffuse + Material Shininess
+	GLfloat *vertex_data = (GLfloat *)malloc(sizeof(GLfloat) * (numVertices * numRecords));
+	
 	ifstream in(filename);
 	if (!in) {
 		cout << "Invalid file";
 	}
 
-	string line;
-	while (getline(in, line)) {
-
+	float value;
+	float obj_values[24];
+	while (in >> value) {
+		obj_values.append(value);
 	}
-	// int numVertices = 0;
-	// int numRecords = 24; 
-	// vector<float> colour[4] = {1.0,1.0,1.0,1.0};
 
-	// //3 + 4 + 2 + 3 + 4 + 4 + 1
-	// // Vertex + Colour + Texture + normals + Material Specular + Material Diffuse + Material Shininess
-	// GLfloat *vertex_data = (GLfloat *)malloc(sizeof(GLfloat) * (numVertices * numRecords));
-	
-	// //loading objects
-	// vector<glm::vec3> obj_verticies;
-	// vector<glm::vec4> obj_amb;
-	// vector<glm::vec2> obj_texture;
-	// vector<glm::vec3> obj_normals;
-	// vector<glm::vec3> obj_spec;
-	// vector<glm::vec3> obj_dif;
-	// float illumination;
-	// vector<glm::vec3 translucency;
+	return obj_values;
 
-	// ifstream inObj(path);
-	// if (!inObj){
-	// 	cout << "Cannot open" << path << endl; exit(1);
-	// }
-
-	// string line;
-	// while(getline(inObj, line)) {
-	// 	if (line.substr(0,2) == "v ") {
-    //         istringstream s(line.substr(2));
-    //         glm::vec3 v; s >> v.x; s >> v.y; s >> v.z = 1.0f;
-	// 		obj_vertices.push_back(v);
-	// 		numVertices += 1;
-	// 	}else if (line.substr(0,3) == "vt ") {
-    //         istringstream s(line.substr(3));
-    //         glm::vec2 t; s >> t.x; s >> t.y = 1.0f;
-    //         obj_vertices.push_back(t);
-	// 	}else if (line.substr(0,3) == "vn ") {
-    //         istringstream s(line.substr(3));
-    //         glm::vec3 n; s >> n.x; s >> n.y; s >> n.z = 1.0f;
-    //         obj_normals.push_back(n);
-	// 	}
-	// } 
-
-	// ifstream inMTL(path.append(".mtl"));
-	// if (!inMTL) {
-	// 	cout << "Cannot open" << path.append(".mtl") << endl; exit(1);
-	// }
-
-	// string line1;
-	// while(getline(inMTL, line1)) {
-	// 	if (line.substr(0,3) == "Kd "){
-	// 		istringstream s(line.substr(3));
-    //         glm::vec3 d; s >> d.x; s >> d.y; s >> d.z = 1.0f;
-    //         obj_dif.push_back(d);
-	// 	}else if (line.substr(0,3) == "Ka ") {
-    //         istringstream s(line.substr(3));
-    //         glm::vec3 a; s >> a.x; s >> a.y; s >> a.z = 1.0f;
-    //         obj_amb.push_back(n);
-	// 	}else if (line.substr(0,3) == "Tf ") {
-    //         istringstream s(line.substr(3));
-    //         translucency << s;
-    //         obj_normals.push_back(n);
-	// 	}else if (line.substr(0,6) == "illum ") {
-    //         istringstream s(line.substr(6));
-    //         illumination << s;
-    //         obj_normals.push_back(n);
-	// 	}
-	// }
-
-	// for (int i = 0; i < numVertices; )
-
-
-
-	// return vertex_data;
 };
 
 GLfloat*  objLoader::loadObj(std::string fileName){
