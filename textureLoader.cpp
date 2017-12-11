fd7745bbcf962ad77845f0c65e4b78ef31c7d59b
Brandon Roberts <brandonnickroberts@gmail.com>
2017-12-11 15:41:23 -0500

    Texture Loading Stub

---
+++ textureLoader.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+#include "textureLoader.hpp"
+
+//This is taken from a tutorial :D
+GLuint loadTextureFromFile( std::string path)
+{
+    GLuint textureID = 0;
+    return textureID;
+}
---
+++ textureLoader.hpp
@@ -0,0 +1,18 @@
+#ifndef TEXTURELOADER_H
+#define TEXTURELOADER_H
+
+#include "OpenGLImports.hpp"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+
+GLuint loadTextureFromFile( std::string path);
+
+
+#endif
