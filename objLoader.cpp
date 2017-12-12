ccffd609198ed9d65f2ee9f8167eda77e69501ca
Vanessa Truong <truonv1@mcmaster.ca>
2017-12-11 19:34:16 -0500

    ObjLoader

--- objLoader.cpp
+++ objLoader.cpp
@@ -6,8 +6,6 @@
 #include <algorithm>
 #include <fstream>
 #include <vector>
-#include <ifstream>
-#include <ofstream>
 #include <iostream>
 #include <list>
 
@@ -15,35 +13,43 @@
 
 using namespace std;
 
-objLoader::objLoader(){
+objLoader::objLoader(){}
 
-}
-GLfloat* objLoader::loadObjFromFile(string filename){
+GLfloat* objLoader::loadObjFromFile(const char* filename){
 	int numVertices = 0;
 	int numRecords = 24; 
-	vector<float> colour[4] = {1.0,1.0,1.0,1.0};
 
 	//3 + 4 + 2 + 3 + 4 + 4 + 1
 	// Vertex + Colour + Texture + normals + Material Specular + Material Diffuse + Material Shininess
 	GLfloat *vertex_data = (GLfloat *)malloc(sizeof(GLfloat) * (numVertices * numRecords));
 	
 	ifstream in(filename);
+
+	float number;
+	char c;
+
 	if (!in) {
 		cout << "Invalid file";
 	}
-
-	float value;
-	float obj_values[24];
-	while (in >> value) {
-		obj_values.append(value);
+	
+	while(!in.eof()) {
+		in.get(c);
+		if (c == ' ') {
+			numVertices += 1;
+		}
 	}
 
-	return obj_values;
+	while (in >> number) {
+		for (int i = 0; i < numVertices; ++i) {
+			vertex_data[i] = number;
+		}
+	}
 
+	return vertex_data;
 };
 
-GLfloat*  objLoader::loadObj(std::string fileName){
+GLfloat*  objLoader::loadObj(const char* path){
 	//Given "example.obj", open "GameAssets/obj/example.obj" using loadTextureFromFile;
 	//Don't forget the mtl file
-	return this->loadObjFromFile(fileName);
+	return this->loadObjFromFile(path);
 };
